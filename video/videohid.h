efd30e24b14467278a34118093a8fdd6e5957177
kevin <kevin@beets3d.cn>
2024-07-24 19:06:32 +0800

    Support change capture resolution and fps

--- video/videohid.h
+++ video/videohid.h
@@ -2,7 +2,12 @@
 #define VIDEOHID_H
 
 #include <QObject>
+
+#ifdef _WIN32
 #include <windows.h> 
+#elif __linux__
+#include <linux/hid.h>
+#endif
 
 class VideoHid : public QObject
 {
@@ -22,6 +27,7 @@ public:
     void stop();
     //get resolution
     QPair<int, int> getResolution();
+    float getFps();
     bool getGpio0();
 
 
@@ -29,11 +35,18 @@ private:
     explicit VideoHid(QObject *parent = nullptr);
 
     QString extractPortNumberFromPath(const QString& path);
-    bool sendFeatureReport(BYTE reportId, BYTE* reportBuffer, DWORD bufferSize);
-    bool getFeatureReport(BYTE reportId, BYTE* reportBuffer, DWORD bufferSize);
     QPair<QByteArray, bool> usbXdataRead4Byte(quint16 u16_address);
     std::wstring GetHIDDevicePath();
     QString devicePath;
+    
+#ifdef _WIN32
+    bool sendFeatureReport(BYTE reportId, BYTE* reportBuffer, DWORD bufferSize);
+    bool getFeatureReport(BYTE reportId, BYTE* reportBuffer, DWORD bufferSize);
+#elif __linux__
+    bool sendFeatureReport(int fd, unsigned char* reportBuffer, int bufferSize);
+    bool getFeatureReport(int fd, unsigned char* reportBuffer, int bufferSize);
+#endif
+
 };
 
 #endif // VIDEOHID_H
