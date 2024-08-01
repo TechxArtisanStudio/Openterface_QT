66c7e695e6153da64fb26c51e0af495cd9aa5fcf
Chow411 <2604590430@qq.com>
2024-08-01 14:52:03 +0800

    finish the left Qwidget tree

--- ui/settingdialog.cpp
+++ ui/settingdialog.cpp
@@ -2,14 +2,27 @@
 #include "ui_settingdialog.h"
 #include "global.h"
 
-#include <QApplication>
+#include <QComboBox>
+#include <QDialogButtonBox>
+#include <QDir>
+#include <QFileDialog>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+#include <QRegularExpression>
+#include <QToolButton>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
 
-
-settingDialog::settingDialog(QWidget *parent)
+settingDialog::settingDialog( QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::settingDialog)
+    , settingTree(new QTreeWidget(this))
 {
     ui->setupUi(this);
+    createSettingTree();
+    createLayout();
+    setWindowTitle(tr("Setting"));
 }
 
 settingDialog::~settingDialog()
@@ -17,19 +30,23 @@ settingDialog::~settingDialog()
     delete ui;
 }
 
-void settingDialog::init(){
-    // find all the widget
-    
-    // just show the setting
-}
-
-void settingDialog::clickbtn()
-{
-    qDebug() << "click btn";
-    QWidget *logwidget = findChild<QWidget*>("logWidget");
-    if (logwidget){
-        qDebug() << "logwidget has been hidden";
-        logwidget->hide();
+void settingDialog::createSettingTree(){
+    qDebug()<< "creating setting Tree";
+    settingTree->setColumnCount(1);
+    // qDebug()<< "end create";
+    settingTree->setHeaderLabels(QStringList(tr("Setting")));
+    settingTree->setSelectionMode(QAbstractItemView::SingleSelection);
+    settingTree->setRootIsDecorated(false);
+    QStringList names = {"log", "video", "audio"};
+    for (const QString &name : names){
+        QTreeWidgetItem *item = new QTreeWidgetItem(settingTree);
+        item->setText(0, name);
     }
-}
+}   // createSettingTree
 
+void settingDialog::createLayout() {
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(settingTree);
+    setLayout(mainLayout);
+
+}   // createLayout
