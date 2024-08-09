bf0058a7cc8d3f7966077ab5705c919e98e9b9a7
ian <13424055869@163.com>
2024-08-09 14:35:00 +0800

    add license restruct the setting dialog code

--- ui/settingdialog.h
+++ ui/settingdialog.h
@@ -1,3 +1,25 @@
+/*
+* ========================================================================== *
+*                                                                            *
+*    This file is part of the Openterface Mini KVM App QT version            *
+*                                                                            *
+*    Copyright (C) 2024   <info@openterface.com>                             *
+*                                                                            *
+*    This program is free software: you can redistribute it and/or modify    *
+*    it under the terms of the GNU General Public License as published by    *
+*    the Free Software Foundation version 3.                                 *
+*                                                                            *
+*    This program is distributed in the hope that it will be useful, but     *
+*    WITHOUT ANY WARRANTY; without even the implied warranty of              *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        *
+*    General Public License for more details.                                *
+*                                                                            *
+*    You should have received a copy of the GNU General Public License       *
+*    along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                            *
+* ========================================================================== *
+*/
+
 #ifndef SETTINGDIALOG_H
 #define SETTINGDIALOG_H
 
@@ -24,15 +46,23 @@ private:
     Ui::SettingDialog *ui;
     QTreeWidget *settingTree;
     QStackedWidget *stackedWidget;
+    QWidget *logPage;
+    QWidget *videoPage;
+    QWidget *audioPage;
     QWidget *buttonWidget;
 
     void switchWidgetShow(QString &btnName);
     void createSettingTree();
     void createLayout();
+    void createLogPage();
+    void createAudioPage();
+    void createVideoPage();
     void createPages();
+    
     void changePage(QTreeWidgetItem *current, QTreeWidgetItem *previous);
     void createButtons();
     void readCheckBoxState();
+    void applyAccrodingPage();
     void setLogCheckBox();
     void handleOkButton();
 };
