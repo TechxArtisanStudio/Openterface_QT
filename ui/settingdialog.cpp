8f314bfc8ccd4badf78f2bc33ea194d4b045b250
Chow411 <2604590430@qq.com>
2024-08-09 14:35:00 +0800

    add license restruct the setting dialog code

--- ui/settingdialog.cpp
+++ ui/settingdialog.cpp
@@ -1,5 +1,29 @@
+/*
+* ========================================================================== *
+*                                                                            *
+*    This file is part of the Openterface Mini KVM App QT version            *
+*                                                                            *
+*    Copyright (C) 2024   <info@openterface.com>                             *
+*                                                                            *
+*    This program is free software: you can redistribute it and/or modify    *
+*    it under the terms of the GNU General Public License as published by    *
+*    the Free Software Foundation version 3.                                 *
+*                                                                            *
+*    This program is distributed in the hope that it will be useful, but     *
+*    WITHOUT ANY WARRANTY; without even the implied warranty of              *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        *
+*    General Public License for more details.                                *
+*                                                                            *
+*    You should have received a copy of the GNU General Public License       *
+*    along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                            *
+* ========================================================================== *
+*/
+
 #include "settingdialog.h"
 #include "ui_settingdialog.h"
+#include "ui/fpsspinbox.h"
+#include "videosettings.h"
 
 #include <QComboBox>
 #include <QDialogButtonBox>
@@ -17,12 +41,16 @@
 #include <QDebug>
 #include <QLoggingCategory>
 
+
 SettingDialog::SettingDialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::SettingDialog)
     , settingTree(new QTreeWidget(this))
     , stackedWidget(new QStackedWidget(this))
     , buttonWidget(new QWidget(this))
+    , logPage(nullptr)
+    , videoPage(nullptr)
+    , audioPage(nullptr)
 {
     ui->setupUi(this);
     createSettingTree();
@@ -52,20 +80,17 @@ void SettingDialog::createSettingTree() {
     settingTree->setMaximumSize(QSize(120, 1000));
     settingTree->setRootIsDecorated(false);
     
-    QStringList names = {"Log"};
-    // QStringList names = {"Log", "Video", "Audio"};
+    // QStringList names = {"Log"};
+    QStringList names = {"Log", "Video", "Audio"};
     for (const QString &name : names) {     // add item to setting tree
         QTreeWidgetItem *item = new QTreeWidgetItem(settingTree);
         item->setText(0, name);
     }
 }
 
-void SettingDialog::createPages() {
-    // Create pages for each setting
-    QWidget *logPage = new QWidget();
-    QWidget *videoPage = new QWidget();
-    QWidget *audioPage = new QWidget();
-    
+void SettingDialog::createLogPage() {
+    logPage = new QWidget();
+
     // Create checkbox for log
     QCheckBox *coreCheckBox = new QCheckBox("Core");
     QCheckBox *serialCheckBox = new QCheckBox("Serial");
@@ -82,30 +107,95 @@ void SettingDialog::createPages() {
     logCheckboxLayout->addWidget(uiCheckBox);
     logCheckboxLayout->addWidget(hostCheckBox);
 
-    // Create labels for each page
     QLabel *logLabel = new QLabel("General log setting");
-    QLabel *videoLabel = new QLabel("General video setting");
-    QLabel *audioLabel = new QLabel("General audio setting");
-    
-    // Create layouts for each page and add labels to them
+
     QVBoxLayout *logLayout = new QVBoxLayout(logPage);
     logLayout->addWidget(logLabel);
     logLayout->addLayout(logCheckboxLayout);
     logLayout->addStretch();
+}
+
+void SettingDialog::createVideoPage() {
+    videoPage = new QWidget();
+
+    QLabel *videoLabel = new QLabel("General video setting");
+    QLabel *resolutionsLabel = new QLabel("Capture resolutions: ");
+    QComboBox *videoFormatBox = new QComboBox();
+
+    QLabel *framerateLabel = new QLabel("Framerate: ");
+    FpsSpinBox *fpsSpinBox = new FpsSpinBox();
+    fpsSpinBox->setObjectName("fpsSpinBox");
+    QSlider *fpsSlider = new QSlider();
+    fpsSlider->setObjectName("fpsSlider");
+    fpsSlider->setOrientation(Qt::Horizontal);
+
+    QHBoxLayout *hBoxLayout = new QHBoxLayout();
+    hBoxLayout->addWidget(fpsSpinBox);
+    hBoxLayout->addWidget(fpsSlider);
+
+    QLabel *formatLabel = new QLabel("Pixel format: ");
+    QComboBox *pixelFormatBox = new QComboBox();
+    pixelFormatBox->setObjectName("pixelFormatBox");
 
     QVBoxLayout *videoLayout = new QVBoxLayout(videoPage);
     videoLayout->addWidget(videoLabel);
+    videoLayout->addWidget(resolutionsLabel);
+    videoLayout->addWidget(videoFormatBox);
+    videoLayout->addWidget(framerateLabel);
+    videoLayout->addLayout(hBoxLayout);
+    videoLayout->addWidget(formatLabel);
+    videoLayout->addWidget(pixelFormatBox);
     videoLayout->addStretch();
+}
+
+void SettingDialog::createAudioPage() {
+    audioPage = new QWidget();
+
+    QLabel *audioLabel = new QLabel("General audio setting");
+    QLabel *audioCodecLabel = new QLabel("Audio Codec: ");
+    QComboBox *audioCodecBox = new QComboBox();
+    audioCodecBox->setObjectName("audioCodecBox");
+
+    QLabel *audioSampleRateLabel = new QLabel("Sample Rate: ");
+    QSpinBox *audioSampleRateBox = new QSpinBox();
+    audioSampleRateBox->setObjectName("audioSampleRateBox");
+    audioSampleRateBox->setEnabled(false);
+
+    QLabel *qualityLabel = new QLabel("Quality: ");
+    QSlider *qualitySlider = new QSlider();
+    qualitySlider->setObjectName("qualitySlider");
+    qualitySlider->setOrientation(Qt::Horizontal);
+
+    QLabel *fileFormatLabel = new QLabel("File Format: ");
+    QComboBox *containerFormatBox = new QComboBox();
+    containerFormatBox->setObjectName("containerFormatBox");
 
     QVBoxLayout *audioLayout = new QVBoxLayout(audioPage);
     audioLayout->addWidget(audioLabel);
+    audioLayout->addWidget(audioCodecLabel);
+    audioLayout->addWidget(audioCodecBox);
+    audioLayout->addWidget(audioSampleRateLabel);
+    audioLayout->addWidget(audioSampleRateBox);
+    audioLayout->addWidget(qualityLabel);
+    audioLayout->addWidget(qualitySlider);
+    audioLayout->addWidget(fileFormatLabel);
+    audioLayout->addWidget(containerFormatBox);
     audioLayout->addStretch();
+}
+
+void SettingDialog::createPages() {
+    createLogPage();
+    createVideoPage();
+    createAudioPage();
+
+    
 
     // Add pages to the stacked widget
     stackedWidget->addWidget(logPage);
     stackedWidget->addWidget(videoPage);
     stackedWidget->addWidget(audioPage);
 }
+
 
 void SettingDialog::createButtons(){
     QPushButton *okButton = new QPushButton("OK");
@@ -124,7 +214,7 @@ void SettingDialog::createButtons(){
 
     connect(okButton, &QPushButton::clicked, this, &SettingDialog::handleOkButton);
     connect(cancelButton, &QPushButton::clicked, this, &QDialog::reject);
-    connect(applyButton, &QPushButton::clicked, this, &SettingDialog::readCheckBoxState);
+    connect(applyButton, &QPushButton::clicked, this, &SettingDialog::applyAccrodingPage);
 }
 
 void SettingDialog::createLayout() {
@@ -209,6 +299,25 @@ void SettingDialog::readCheckBoxState() {
     }
 
     QLoggingCategory::setFilterRules(logFilter);
+}
+
+void SettingDialog::applyAccrodingPage(){
+    int *currentPageIndex = stackedWidget->currentIndex();
+    switch (currentPageIndex)
+    {
+        // sequence Log Video Audio
+        case 0:
+            readCheckBoxState();
+            break;
+        case 1:
+            
+            break;
+        case 2:
+
+            break;
+        default:
+            break;
+    }
 }
 
 void SettingDialog::handleOkButton() {
