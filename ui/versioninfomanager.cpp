cdc3870e8b4ccd7b852eb67d7e41e4683df687c2
Chow411 <2604590430@qq.com>
2024-11-14 11:18:39 +0800

    Add the licenses for codes

--- ui/versioninfomanager.cpp
+++ ui/versioninfomanager.cpp
@@ -1,3 +1,26 @@
+/*
+* ========================================================================== *
+*                                                                            *
+*    This file is part of the Openterface Mini KVM App QT version            *
+*                                                                            *
+*    Copyright (C) 2024   <info@openterface.com>                             *
+*                                                                            *
+*    This program is free software: you can redistribute it and/or modify    *
+*    it under the terms of the GNU General Public License as published by    *
+*    the Free Software Foundation version 3.                                 *
+*                                                                            *
+*    This program is distributed in the hope that it will be useful, but     *
+*    WITHOUT ANY WARRANTY; without even the implied warranty of              *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        *
+*    General Public License for more details.                                *
+*                                                                            *
+*    You should have received a copy of the GNU General Public License       *
+*    along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                            *
+* ========================================================================== *
+*/
+
+
 #include "versioninfomanager.h"
 #include <QApplication>
 #include <QClipboard>
@@ -201,8 +224,8 @@ void VersionInfoManager::handleUpdateCheckResponse(QNetworkReply *reply)
         }
     } else {
         QMessageBox::warning(nullptr, 
-                           tr("Update Check Failed"),
-                           tr("Failed to check for updates.\nError: %1")
+                           tr("Update Check Failed\n"),
+                           tr("Failed to check for updates.\nError: %1\nPlease check your internet connection.\n")
                            .arg(reply->errorString()));
     }
 }
