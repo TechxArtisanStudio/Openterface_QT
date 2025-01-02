27af642002213dae1e1447b4f69c46d477338128
ian chow <81250587+Chow411@users.noreply.github.com>
2025-01-02 11:11:02 +0800

    Fix bug (#40)
    These changes enhance the clarity and efficiency of the key processing logic in the application.
    
    * Enhance key mapping and processing in KeyboardMouse and SemanticAnalyzer
    
    - Added uppercase key mappings for all alphabetic characters in KeyboardMouse.h to support case-sensitive key handling.
    - Updated analyzeSendStatement method in semanticAnalyzer.cpp to differentiate between uppercase and lowercase key inputs, incorporating shift key logic for proper character representation.
    
    These changes improve the accuracy and functionality of keyboard input handling in the application.
    
    * del some useless code
    
    * Fix the upper char can't recongize in send command

--- scripts/semanticAnalyzer.cpp
+++ scripts/semanticAnalyzer.cpp
@@ -123,7 +123,7 @@ void SemanticAnalyzer::analyzeLockState(const CommandStatementNode* node, const 
             general[0] = keydata.value(keyName);
             keyPacket pack(general);
             keyboardMouse->addKeyPacket(pack);
-            keyboardMouse->executeCommand();
+            keyboardMouse->keyboardSend();
         }
     }
     if (offRegex.match(tmpKeys).hasMatch()){
@@ -133,7 +133,7 @@ void SemanticAnalyzer::analyzeLockState(const CommandStatementNode* node, const 
             general[0] = keydata.value(keyName);
             keyPacket pack(general);
             keyboardMouse->addKeyPacket(pack);
-            keyboardMouse->executeCommand();
+            keyboardMouse->keyboardSend();
         }
     }
 }
@@ -161,73 +161,77 @@ void SemanticAnalyzer::analyzeSleepStatement(const CommandStatementNode* node){
 
 void SemanticAnalyzer::analyzeSendStatement(const CommandStatementNode* node) {
     const auto& options = node->getOptions();
-    // Map for special keys
     
     if (options.empty()) {
-        qDebug(log_script) << "No coordinates provided for Send command";
+        qDebug(log_script) << "No keys provided for Send command";
         return;
     }
-    QString tmpKeys;
-    for (const auto& token : options){
-        qDebug(log_script) << QString::fromStdString(token);
-        if (token != "\"") tmpKeys.append(QString::fromStdString(token));
-    }
-    int i = 0;
 
-    std::vector<int> keys;
-    while (i<tmpKeys.length()){
-        const QChar& ch = tmpKeys[i];
-        
+    // Combine all tokens into a single string, excluding quotes
+    QString tmpKeys;
+    for (const auto& token : options) {
+        if (token != "\"") {
+            tmpKeys.append(QString::fromStdString(token));
+        }
+    }
+
+    qDebug(log_script) << "Processing keys:" << tmpKeys;
+
+    int pos = 0;
+    while (pos < tmpKeys.length()) {
         std::array<uint8_t, 6> general = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
         uint8_t control = 0x00;
-        if (ch != '{' && !controldata.contains(ch)){
-            general[0] = keydata.value(ch);
-        } else if(ch  == '{'){
-            qDebug(log_script) << "General data";
-            extractKeyFromBrace(tmpKeys, i, general);
-        } else if (controldata.contains(ch))
-        {
-            qDebug(log_script) << "control data";
-            int index = 0;
-            control = controldata.value(ch);
-            for (int j = i+1; j < tmpKeys.length(); j++){
-                if (tmpKeys[j] == '{'){
-                    extractKeyFromBrace(tmpKeys, j, general, index);
-                    index += 1;
-                }else if(controldata.contains(tmpKeys[j])){
-                    general[index] = keydata.value(tmpKeys[j]);
-                    index += 1;
+        
+        // Check for control characters first
+        QRegularExpressionMatch controlMatch = controlKeyRegex.match(tmpKeys, pos);
+        if (controlMatch.hasMatch() && controlMatch.capturedStart() == pos) {
+            // Process control key sequence
+            QString controlChar = controlMatch.captured(1);
+            QString keys = controlMatch.captured(2);
+            control = controldata.value(controlChar[0]);
+            
+            // Process the keys after the control character
+            int keyIndex = 0;
+            int keyPos = 0;
+            while (keyPos < keys.length() && keyIndex < 6) {
+                if (keys[keyPos] == '{') {
+                    // Handle braced key
+                    QRegularExpressionMatch braceMatch = braceKeyRegex.match(keys, keyPos);
+                    if (braceMatch.hasMatch()) {
+                        QString keyName = braceMatch.captured(1);
+                        general[keyIndex++] = keydata.value(keyName);
+                        keyPos = braceMatch.capturedEnd();
+                        continue;
+                    }
+                }
+                // Handle single character
+                general[keyIndex++] = keydata.value(keys[keyPos]);
+                keyPos++;
+            }
+            pos = controlMatch.capturedEnd();
+        } else {
+            // Check for braced keys
+            QRegularExpressionMatch braceMatch = braceKeyRegex.match(tmpKeys, pos);
+            if (braceMatch.hasMatch() && braceMatch.capturedStart() == pos) {
+                QString keyName = braceMatch.captured(1);
+                general[0] = keydata.value(keyName);
+                pos = braceMatch.capturedEnd();
+            } else {
+                // Handle single character
+                if (tmpKeys[pos].isUpper()){
+                    control = 0x02;     // shift press let the char become upper while send data
                 }else{
-                    general[index] = keydata.value(tmpKeys[j]);
-                    i = j;
-                    break;
+                    general[0] = keydata.value(tmpKeys[pos]);
+                    pos++;
                 }
             }
         }
-        keyPacket pack(general,control);
+
+        keyPacket pack(general, control);
         keyboardMouse->addKeyPacket(pack);
-        i++;
     }
 
-    keyboardMouse->executeCommand();
-    // for (int i =0; i<keys.size(); i++){
-    //     keyboardManager->handleKeyboardAction(keys[i], 0, true);
-    //     keyboardManager->handleKeyboardAction(keys[i], 0, false);
-    // }
-
-}
-
-void SemanticAnalyzer::extractKeyFromBrace(const QString& tmpKeys, int& i, std::array<uint8_t, 6>& general, int genral_index){
-    QString tmpkey;
-    for (int j = i + 1; j < tmpKeys.length(); j++) {
-        if (tmpKeys[j] != '}') {
-            tmpkey.append(tmpKeys[j]);
-        } else {
-            general[genral_index] = keydata.value(tmpkey);
-            i = j;
-            break;
-        }
-    }
+    keyboardMouse->keyboardSend();
 }
 
 void SemanticAnalyzer::analyzeClickStatement(const CommandStatementNode* node) {
