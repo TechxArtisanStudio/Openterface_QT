ecd7ce42b594350c6ada12d79c60bb52854c1cc6
Kevin Peng <kevinzjpeng@gmail.com>
2025-02-08 20:09:11 +0800

    Add Japanese keyboard Layout

--- target/KeyboardLayouts.cpp
+++ target/KeyboardLayouts.cpp
@@ -14,6 +14,7 @@ const KeyboardLayoutConfig QWERTY_UK("UK QWERTY", false);
 const KeyboardLayoutConfig AZERTY_FR("French AZERTY", false);
 const KeyboardLayoutConfig QWERTZ_DE("German QWERTZ", false);
 const KeyboardLayoutConfig QWERTZ_DK("Danish QWERTY", false);
+const KeyboardLayoutConfig JAPANESE("Japanese", false);
 
 // Initialize all layouts
 struct LayoutInitializer {
