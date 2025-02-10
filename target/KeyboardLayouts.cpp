7421ca358b7fe65ae83397b72d101ef3491ef0ec
Kevin Peng <kevin@beets3d.cn>
2025-02-10 00:54:38 -0800

    Refactor the keyboard layout setting (#151)

--- target/KeyboardLayouts.cpp
+++ target/KeyboardLayouts.cpp
@@ -23,6 +23,8 @@ struct LayoutInitializer {
     }
 } layoutInitializer;
 
+QMap<QString, int> KeyboardLayoutConfig::keyNameToQt;
+
 // Static method implementation
 KeyboardLayoutConfig KeyboardLayoutConfig::fromJsonFile(const QString& filePath) {
     KeyboardLayoutConfig config;
@@ -53,91 +55,9 @@ KeyboardLayoutConfig KeyboardLayoutConfig::fromJsonFile(const QString& filePath)
     qCDebug(log_keyboard_layouts) << "Loading layout:" << config.name;
 
     // Create a mapping from key names to Qt key codes
-    QMap<QString, int> keyNameToQt;
-    keyNameToQt["A"] = Qt::Key_A;
-    keyNameToQt["B"] = Qt::Key_B;
-    keyNameToQt["C"] = Qt::Key_C;
-    keyNameToQt["D"] = Qt::Key_D;
-    keyNameToQt["E"] = Qt::Key_E;
-    keyNameToQt["F"] = Qt::Key_F;
-    keyNameToQt["G"] = Qt::Key_G;
-    keyNameToQt["H"] = Qt::Key_H;
-    keyNameToQt["I"] = Qt::Key_I;
-    keyNameToQt["J"] = Qt::Key_J;
-    keyNameToQt["K"] = Qt::Key_K;
-    keyNameToQt["L"] = Qt::Key_L;
-    keyNameToQt["M"] = Qt::Key_M;
-    keyNameToQt["N"] = Qt::Key_N;
-    keyNameToQt["O"] = Qt::Key_O;
-    keyNameToQt["P"] = Qt::Key_P;
-    keyNameToQt["Q"] = Qt::Key_Q;
-    keyNameToQt["R"] = Qt::Key_R;
-    keyNameToQt["S"] = Qt::Key_S;
-    keyNameToQt["T"] = Qt::Key_T;
-    keyNameToQt["U"] = Qt::Key_U;
-    keyNameToQt["V"] = Qt::Key_V;
-    keyNameToQt["W"] = Qt::Key_W;
-    keyNameToQt["X"] = Qt::Key_X;
-    keyNameToQt["Y"] = Qt::Key_Y;
-    keyNameToQt["Z"] = Qt::Key_Z;
-    keyNameToQt["0"] = Qt::Key_0;
-    keyNameToQt["1"] = Qt::Key_1;
-    keyNameToQt["2"] = Qt::Key_2;
-    keyNameToQt["3"] = Qt::Key_3;
-    keyNameToQt["4"] = Qt::Key_4;
-    keyNameToQt["5"] = Qt::Key_5;
-    keyNameToQt["6"] = Qt::Key_6;
-    keyNameToQt["7"] = Qt::Key_7;
-    keyNameToQt["8"] = Qt::Key_8;
-    keyNameToQt["9"] = Qt::Key_9;
-    keyNameToQt["Space"] = Qt::Key_Space;
-    keyNameToQt["Return"] = Qt::Key_Return;
-    keyNameToQt["Tab"] = Qt::Key_Tab;
-    keyNameToQt["Backspace"] = Qt::Key_Backspace;
-    keyNameToQt["Delete"] = Qt::Key_Delete;
-    keyNameToQt["Escape"] = Qt::Key_Escape;
-    keyNameToQt["Shift"] = Qt::Key_Shift;
-    keyNameToQt["Control"] = Qt::Key_Control;
-    keyNameToQt["Alt"] = Qt::Key_Alt;
-    keyNameToQt["CapsLock"] = Qt::Key_CapsLock;
-    keyNameToQt["Minus"] = Qt::Key_Minus;
-    keyNameToQt["Equal"] = Qt::Key_Equal;
-    keyNameToQt["BracketLeft"] = Qt::Key_BracketLeft;
-    keyNameToQt["BracketRight"] = Qt::Key_BracketRight;
-    keyNameToQt["Backslash"] = Qt::Key_Backslash;
-    keyNameToQt["Semicolon"] = Qt::Key_Semicolon;
-    keyNameToQt["Apostrophe"] = Qt::Key_Apostrophe;
-    keyNameToQt["QuoteLeft"] = Qt::Key_QuoteLeft;
-    keyNameToQt["Comma"] = Qt::Key_Comma;
-    keyNameToQt["Period"] = Qt::Key_Period;
-    keyNameToQt["Slash"] = Qt::Key_Slash;
-    keyNameToQt["Hash"] = Qt::Key_NumberSign;
-
-    // Function keys
-    keyNameToQt["F1"] = Qt::Key_F1;
-    keyNameToQt["F2"] = Qt::Key_F2;
-    keyNameToQt["F3"] = Qt::Key_F3;
-    keyNameToQt["F4"] = Qt::Key_F4;
-    keyNameToQt["F5"] = Qt::Key_F5;
-    keyNameToQt["F6"] = Qt::Key_F6;
-    keyNameToQt["F7"] = Qt::Key_F7;
-    keyNameToQt["F8"] = Qt::Key_F8;
-    keyNameToQt["F9"] = Qt::Key_F9;
-    keyNameToQt["F10"] = Qt::Key_F10;
-    keyNameToQt["F11"] = Qt::Key_F11;
-    keyNameToQt["F12"] = Qt::Key_F12;
-
-    // Navigation keys
-    keyNameToQt["Up"] = Qt::Key_Up;
-    keyNameToQt["Down"] = Qt::Key_Down;
-    keyNameToQt["Left"] = Qt::Key_Left;
-    keyNameToQt["Right"] = Qt::Key_Right;
-    keyNameToQt["PageUp"] = Qt::Key_PageUp;
-    keyNameToQt["PageDown"] = Qt::Key_PageDown;
-    keyNameToQt["Home"] = Qt::Key_Home;
-    keyNameToQt["End"] = Qt::Key_End;
-    keyNameToQt["Insert"] = Qt::Key_Insert;
-    keyNameToQt["Delete"] = Qt::Key_Delete;
+    if (keyNameToQt.isEmpty()) {
+        initializeKeyNameToQt(keyNameToQt);
+    }
 
     // Load key map
     QJsonObject keyMap = json["key_map"].toObject();
@@ -164,21 +84,13 @@ KeyboardLayoutConfig KeyboardLayoutConfig::fromJsonFile(const QString& filePath)
         if (ok && qtKey != Qt::Key_unknown) {
             config.keyMap[qtKey] = scanCode;
             qCDebug(log_keyboard_layouts) << "Successfully mapped key" << keyName 
-                                        << "(" << qtKey << ") to scancode 0x" 
-                                        << QString::number(scanCode, 16);
+                                        << "(" << qtKey << ") to scancode 0x" + QString::number(scanCode, 16);
         } else {
             qCWarning(log_keyboard_layouts) << "Failed to map key" << keyName 
                                           << "value:" << valueStr 
                                           << "ok:" << ok 
                                           << "qtKey:" << qtKey;
         }
-    }
-
-    qCDebug(log_keyboard_layouts) << "Final keyMap size:" << config.keyMap.size();
-    qCDebug(log_keyboard_layouts) << "KeyMap contents:";
-    for (auto it = config.keyMap.begin(); it != config.keyMap.end(); ++it) {
-        qCDebug(log_keyboard_layouts) << "  Qt key:" << it.key() 
-                                    << "-> Scancode: 0x" << QString::number(it.value(), 16);
     }
 
     // Load char mapping
@@ -267,4 +179,4 @@ KeyboardLayoutConfig KeyboardLayoutManager::getLayout(const QString& name) const
 
 QStringList KeyboardLayoutManager::getAvailableLayouts() const {
     return layouts.keys();
-} 
+}
