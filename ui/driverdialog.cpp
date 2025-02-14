4c9cfffca46445ab4769d3971a920af357752166
Kevin Peng <kevin@beets3d.cn>
2025-02-14 00:39:37 -0800

    Fix the driver detection fail in windows (#168)
    * Change to use pnputil to install driver
    
    * Try to change to use MinGW instead of MSVC compiler
    
    * Fix build error
    
    * Remove the MSVC dependency
    
    * fix the qmake path issue
    
    * Fix path issue
    
    * Update cache key
    
    * Update key
    
    * Remove some unnecessary dll
    
    * Fix the iss file format issue
    
    * Add standalone build
    
    * fix package error
    
    * fix fail to buld
    
    * Add cache to standalone build
    
    * Remove platform
    
    * Add platforms plugin
    
    * Remove release
    
    * Chnage to ARTIFACT name
    
    * Build static QT
    
    * Add better cache
    
    * Build from source again
    
    * sync
    
    * echo on
    
    * Add ninja
    
    * sync
    
    * sync
    
    * Set path
    
    * Set path in the build from source bat
    
    * Add more path
    
    * Add mingw bin path
    
    * remove g++ checking
    
    * Add static build flag
    
    * fix path again
    
    * Make the build cache
    
    * Update standalone-build.yaml
    
    Trail test
    
    * Update standalone-build.yaml
    
    Fix the path name error
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * fix standalone build format issue
    
    * check qt path
    
    * fix qmake path
    
    * Enhance standalone build workflow with MinGW and Qt configuration checks
    
    * Remove FFmpeg and PulseAudio build configuration options
    
    * Temporarily disable standalone build steps in GitHub Actions workflow
    
    * Remove exclude
    
    * Resume build test
    
    * Build again
    
    * Quick fix for build static
    
    * build again
    
    * Build again
    
    * Build again
    
    * Build again
    
    * add --no-translations
    
    * Add  --verbose
    
    * add verbose level
    
    * auto change .pro
    
    * fix .pro
    
    * change qmake
    
    * change again
    
    * change again
    
    * Reset error level
    
    * Check path
    
    * change to windeployqt6
    
    * remove windeployqt
    
    * add  -static-libgcc -static-libstdc++
    
    * add pthread too
    
    * add mingw-w64
    
    * check path
    
    * check all
    
    * check mingw only
    
    * add mingw-64 source
    
    * use MSYS2
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Update standalone-build.yaml
    
    * Change MingGW installation method
    
    * Change mingw-w64.zip link
    
    * Sync
    
    * Remove find static lib
    
    * Enable the build code
    
    * Add test build
    
    * sync
    
    * find .a
    
    * Change path
    
    * fix add QMAKE_LFLAGS for static build
    
    * Improve cache
    
    * add verbose
    
    * Add find LFLAGS
    
    * Change findstr
    
    * sync
    
    * Add QMAKE_LFLAGS
    
    * build again
    
    * cat Makefile
    
    * sync
    
    * change QMAKE_LFLAGS
    
    * Add in LIBS
    
    * add -static
    
    * add file libwinpthread.a
    
    * Rollback
    
    * Improve build env
    
    * Add driver check
    
    * Add auto install driver logic in windows
    
    * Fix driver install

--- ui/driverdialog.cpp
+++ ui/driverdialog.cpp
@@ -47,9 +47,40 @@ void DriverDialog::accept()
     // Implement logic for when OK is clicked
     QMessageBox::information(this, "Information", "Proceeding with driver installation.");
     
+    // Log the driver installation attempt
+    std::cout << "Attempting to install driver using pnputil." << std::endl;
+
     // Execute pnputil to install the driver
     QProcess::execute("pnputil.exe", QStringList() << "/add-driver" << "CH341SER.INF" << "/install");
     
+    // Log the completion of the installation attempt
+    std::cout << "Driver installation command executed." << std::endl;
+
+    // Prompt user to restart computer
+    QMessageBox::StandardButton reply = QMessageBox::question(
+        this,
+        "Restart Required",
+        "The driver has been installed. A system restart is required for the changes to take effect.\n\n"
+        "Would you like to restart your computer now?",
+        QMessageBox::Yes | QMessageBox::No
+    );
+
+    if (reply == QMessageBox::Yes) {
+#ifdef _WIN32
+        // Execute system restart command
+        QProcess::startDetached("shutdown", QStringList() << "-r" << "-t" << "0");
+#elif defined(__linux__)
+        // For Linux systems
+        QProcess::startDetached("reboot");
+#endif
+    } else {
+        QMessageBox::information(
+            this,
+            "Restart Later",
+            "Please remember to restart your computer for the driver to work properly."
+        );
+    }
+
     // Call the base class accept method to close the dialog
     QDialog::accept();
 }
@@ -62,30 +93,79 @@ void DriverDialog::reject()
 
 bool DriverDialog::isDriverInstalled() {
 #ifdef _WIN32 // Check if compiling on Windows
-    // Use SetupDiGetClassDevs to get a list of devices
-    HDEVINFO deviceInfoSet = SetupDiGetClassDevs(NULL, L"USB\\VID_1A86&PID_7523", NULL, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+    // Log the start of the driver check
+    std::cout << "Checking if driver is installed on Windows." << std::endl;
+
+    // Get the GUID for USB devices
+    const GUID GUID_DEVINTERFACE_USB_DEVICE = { 0xA5DCBF10, 0x6530, 0x11D2, {0x90, 0x1F, 0x00, 0xC0, 0x4F, 0xB9, 0x51, 0xED} };
+
+    // Use SetupDiGetClassDevs to get a list of USB devices
+    HDEVINFO deviceInfoSet = SetupDiGetClassDevs(
+        &GUID_DEVINTERFACE_USB_DEVICE,
+        NULL,
+        NULL,
+        DIGCF_PRESENT | DIGCF_DEVICEINTERFACE
+    );
+
     if (deviceInfoSet == INVALID_HANDLE_VALUE) {
-        return false; // Error occurred
+        DWORD error = GetLastError();
+        std::cout << "Error occurred while getting device info set. Error code: " << error << std::endl;
+        return false;
     }
 
-    // Check if any devices are present
     SP_DEVINFO_DATA deviceInfoData;
     deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+    // Buffer for device hardware ID
+    WCHAR hwIdBuffer[256];
+
+    // Enumerate all devices
     for (DWORD i = 0; SetupDiEnumDeviceInfo(deviceInfoSet, i, &deviceInfoData); i++) {
-        // Here you can check the device's properties if needed
-        // For now, we just return true if we find any device
-        SetupDiDestroyDeviceInfoList(deviceInfoSet);
-        return true; // Driver is installed
+        // Get the hardware ID
+        if (SetupDiGetDeviceRegistryPropertyW(
+            deviceInfoSet,
+            &deviceInfoData,
+            SPDRP_HARDWAREID,
+            NULL,
+            (PBYTE)hwIdBuffer,
+            sizeof(hwIdBuffer),
+            NULL))
+        {
+            std::wcout << L"Found device with hardware ID: " << hwIdBuffer << std::endl;
+            
+            // Check if this is our device
+            if (wcsstr(hwIdBuffer, L"USB\\VID_1A86&PID_7523") != NULL) {
+                std::cout << "Found matching USB device!" << std::endl;
+                SetupDiDestroyDeviceInfoList(deviceInfoSet);
+                return true;
+            }
+        }
+    }
+
+    // Check if last error was just end of list
+    DWORD error = GetLastError();
+    if (error != ERROR_NO_MORE_ITEMS) {
+        std::cout << "Error while enumerating devices. Error code: " << error << std::endl;
+    } else {
+        std::cout << "No matching device found." << std::endl;
     }
 
     SetupDiDestroyDeviceInfoList(deviceInfoSet);
-    return false; // Driver not found
+    return false;
+
 #elif defined(__linux__) // Check if compiling on Linux
+    // Log the start of the driver check
+    std::cout << "Checking if driver is installed on Linux." << std::endl;
+
     // Check if the driver is loaded by looking for the device file
     std::ifstream deviceFile("/dev/ttyUSB0"); // Adjust the path as necessary for your driver
-    return deviceFile.good(); // Returns true if the device file exists
+    bool isInstalled = deviceFile.good();
+    std::cout << "Driver installation status: " << (isInstalled ? "Installed" : "Not Installed") << std::endl;
+    return isInstalled; // Returns true if the device file exists
 #else
     // Implement logic for other platforms if needed
+    std::cout << "Driver check not implemented for this platform." << std::endl;
+
     return false; // Assume not installed for non-Windows and non-Linux platforms
 #endif
 }
