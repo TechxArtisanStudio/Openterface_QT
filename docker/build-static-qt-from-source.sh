77485825a8beea67deef028e3a5579511fa732f7
Kevin Peng <kevinzjpeng@gmail.com>
2025-02-18 14:52:40 +0800

    Fix libxkbcommon Meson configuration dependency linking
    - Correct syntax for xdmcp dependency insertion
    - Adjust dependency linking order in Meson configuration
    - Remove extraneous comma in dependency configuration

--- docker/build-static-qt-from-source.sh
+++ docker/build-static-qt-from-source.sh
@@ -425,11 +425,11 @@ if [ -f "$LIBXKBCOMMON_MESON_FILE" ]; then
     sed -i "s/\(xcb_dep = dependency('xcb'\),[^)]*)/\1)/" "$LIBXKBCOMMON_MESON_FILE"
     sed -i "s/\(xcb_xkb_dep = dependency('xcb-xkb'\),[^)]*)/\1)/" "$LIBXKBCOMMON_MESON_FILE"
     sed -i "/xcb_xkb_dep = dependency('xcb-xkb')/axau_dep = dependency('xau', static: true)" "$LIBXKBCOMMON_MESON_FILE"
-    sed -i "/xau_dep = ependency('xau', static: true)/axdmcp_dep = dependency('xdmcp', static: true)    ," "$LIBXKBCOMMON_MESON_FILE" 
+    sed -i "/xau_dep = dependency('xau', static: true)/axdmcp_dep = dependency('xdmcp', static: true)" "$LIBXKBCOMMON_MESON_FILE" 
 
     # Ensure libXau is linked with xkbcli-interactive-x11
     sed -i "/xcb_xkb_dep,/axau_dep," "$LIBXKBCOMMON_MESON_FILE"
-    sed -i "/xdmcp_dep,/axdmcp_dep," "$LIBXKBCOMMON_MESON_FILE"
+    sed -i "/xau_dep,/axdmcp_dep," "$LIBXKBCOMMON_MESON_FILE"
 else
     echo "Error: $LIBXKBCOMMON_MESON_FILE not found."
 fi
