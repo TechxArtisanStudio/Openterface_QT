c58639d2c9de809bb2daf348d923d7c186be73b1
Göran Roseen <goran@roseen.se>
2025-02-18 08:13:15 +0100

    Added a Swedish keyboard layout as a resource (#163)
    Co-authored-by: Göran Roseen <goran.roseen@nobelprize.org>
    Co-authored-by: ian chow <81250587+Chow411@users.noreply.github.com>

--- target/KeyboardLayouts.cpp
+++ target/KeyboardLayouts.cpp
@@ -14,6 +14,7 @@ const KeyboardLayoutConfig QWERTY_UK("UK QWERTY", false);
 const KeyboardLayoutConfig AZERTY_FR("French AZERTY", false);
 const KeyboardLayoutConfig QWERTZ_DE("German QWERTZ", false);
 const KeyboardLayoutConfig QWERTY_DK("Danish QWERTY", false);
+const KeyboardLayoutConfig QWERTY_SE("Swedish QWERTY", false);
 const KeyboardLayoutConfig JAPANESE("Japanese", false);
 
 // Initialize all layouts
