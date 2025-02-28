de51ce4ac2bd56054c1cda113d53b28b29ffbae4
Kevin Peng <kevinzjpeng@gmail.com>
2025-02-28 18:45:27 +0800

    add libxcb-randr-dev

--- build-script/build-static-qt-from-source.sh
+++ build-script/build-static-qt-from-source.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y build-essential meson ninja-build bison flex pkg-config 
     libxcb-image0-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-render-util0 \
     libxcb-render-util0-dev libxcb-render0-dev libxcb-shm0-dev libxcb-util1 \
     libxfixes-dev libxi-dev libx11-dev libx11-xcb-dev libxext-dev libxfixes-dev \
-    libdbus-1-dev libfontconfig1-dev libfreetype6-dev libxkbcommon-dev libxkbcommon-x11-dev
+    libdbus-1-dev libfontconfig1-dev libfreetype6-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb-randr-dev
 
 QT_VERSION=6.5.3
 QT_MAJOR_VERSION=6.5
