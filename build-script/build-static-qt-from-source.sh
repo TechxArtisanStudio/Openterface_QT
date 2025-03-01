bffe9374248a629727867303e6295d266138e16e
Kevin Peng <kevin@beets3d.cn>
2025-03-01 08:03:05 +0800

    Update build-static-qt-from-source.sh

--- build-script/build-static-qt-from-source.sh
+++ build-script/build-static-qt-from-source.sh
@@ -84,8 +84,8 @@ done
 # Define common CMake flags with size optimization
 CMAKE_COMMON_FLAGS="-Wno-dev -DCMAKE_POLICY_DEFAULT_CMP0177=NEW -DCMAKE_POLICY_DEFAULT_CMP0174=NEW \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_FLAGS=\"-Os -ffunction-sections\" \
-    -DCMAKE_C_FLAGS=\"-Os -ffunction-sections\" \
+    -DCMAKE_CXX_FLAGS=\"-Os\" \
+    -DCMAKE_C_FLAGS=\"-Os\" \
     -DCMAKE_EXE_LINKER_FLAGS=\"-Wl,--gc-sections\" \
     -DCMAKE_SHARED_LINKER_FLAGS=\"-Wl,--gc-sections\" \
     -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON"
