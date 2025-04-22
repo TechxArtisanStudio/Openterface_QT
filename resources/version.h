590b5af7facc422b0776b112c964f9119341b001
GitHub Action <action@github.com>
2025-04-22 10:35:19 +0000

    Bump version to 0.3.5.112

--- resources/version.h
+++ resources/version.h
@@ -5,6 +5,6 @@
 // The patch number resets to 1 whenever the major or minor version is updated.
 // Please refer to update_version.py
 
-#define APP_VERSION "0.3.4.111"
+#define APP_VERSION "0.3.5.112"
 
 #endif // VERSION_H
