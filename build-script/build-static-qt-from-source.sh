58f7e62194c7ed22dcd039e88ecfa5c9ad1d963a
Kevin Peng <kevin@beets3d.cn>
2025-08-21 21:31:07 +0800

    Create the work directory for Qt build process

--- build-script/build-static-qt-from-source.sh
+++ build-script/build-static-qt-from-source.sh
@@ -32,6 +32,7 @@ FFMPEG_PREFIX=/opt/Qt6
 FFMPEG_VERSION="6.1.1"
 GSTREAMER_VERSION="1.22.11"
 WORK_DIR="${HOME}/qt-amd64-build"
+mkdir -p "${WORK_DIR}"
 # Update module list to include qtdeclarative (which provides Qt Quick)
 MODULES=("qtbase" "qtshadertools" "qtdeclarative" "qtmultimedia" "qtsvg" "qtserialport" "qttools")
 DOWNLOAD_BASE_URL="https://download.qt.io/archive/qt/$QT_MAJOR_VERSION/$QT_VERSION/submodules"
