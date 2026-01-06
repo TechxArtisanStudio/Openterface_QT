5253308f5d1f9b7423808e7a9ebd40e014e349ae
Chow411 <2604590430@qq.com>
2026-01-06 11:10:19 +0800

    Update application version to 0.5.9.006

--- resources/version.h
+++ resources/version.h
@@ -5,7 +5,7 @@
 // The patch number resets to 1 whenever the major or minor version is updated.
 // Please refer to build-script/update_version.py
 
-#define APP_VERSION "0.5.9"
+#define APP_VERSION "0.5.9.006"
 #define STABLE_APP_VERSION "0.3.19"
 
 #endif // VERSION_H
