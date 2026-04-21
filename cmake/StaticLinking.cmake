ba83a55e05b7b355472a0345a9a1f3613cb28445
TSTC123 <2735056035@qq.com>
2026-04-21 16:51:32 +0800

    Modify Qt's static build script

--- cmake/StaticLinking.cmake
+++ cmake/StaticLinking.cmake
@@ -43,6 +43,7 @@ function(force_static_compression_libraries target)
         endif()
 
         # PCRE2 is required by Qt6 Core for regular expressions
+        # Qt static build must have been compiled with -DPCRE2_STATIC
         set(PCRE2_LIBRARY "${MINGW_ROOT}/lib/libpcre2-16.a")
         if(EXISTS "${PCRE2_LIBRARY}")
             list(APPEND STATIC_LIB_PATHS "${PCRE2_LIBRARY}")
@@ -50,6 +51,9 @@ function(force_static_compression_libraries target)
         else()
             message(WARNING "  PCRE2 not found at ${PCRE2_LIBRARY} - Qt6 regex will fail to link")
         endif()
+
+        # Define PCRE2_STATIC so our code doesn't generate __imp_ references either
+        target_compile_definitions(${target} PRIVATE PCRE2_STATIC)
         
         # Link found static libraries
         if(STATIC_LIB_PATHS)
