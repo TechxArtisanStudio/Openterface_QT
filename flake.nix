53c441e0032864c5fc63e515757b600139b1abfa
Justinas Stankevičius <justinas@users.noreply.github.com>
2026-08-12 09:13:13 +0300

    Update flake to nixos-26.05 (#568)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Openterface Mini-KVM: Host Applications for Windows and Linux";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
