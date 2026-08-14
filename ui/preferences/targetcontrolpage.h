cf9c887257c793842c765d345a207e1f30a139a9
Chow411 <2604590430@qq.com>
2026-08-14 15:21:50 +0800

    refactor: migrate TargetControlPage to PreferencePageBase

--- ui/preferences/targetcontrolpage.h
+++ ui/preferences/targetcontrolpage.h
@@ -38,17 +38,18 @@
 #include <QButtonGroup>
 #include "host/cameramanager.h"
 #include "fontstyle.h"
+#include "preferencepagebase.h"
 
-class TargetControlPage : public QWidget
+class TargetControlPage : public PreferencePageBase
 {
     Q_OBJECT
 public:
     explicit TargetControlPage(QWidget *parent = nullptr);
     void setupUI();
-    void applyHardwareSetting();
+    void applySettings() override;
     void initHardwareSetting();
-    void captureSnapshot();
-    void revertToSnapshot();
+    void captureSnapshot() override;
+    void revertToSnapshot() override;
 
 private:
     QLabel *hardwareLabel;
@@ -67,7 +68,7 @@ private:
 
     void addCheckBoxLineEditPair(QCheckBox *checkBox, QLineEdit *lineEdit);
     void onCheckBoxStateChanged(Qt::CheckState state);
-    
+
     std::array<bool, 4> extractBits(QString hexString);
     QByteArray convertCheckBoxValueToBytes();
     QMap<QCheckBox *, QLineEdit *> USBCheckBoxEditMap; // map of checkboxes to line edit about VID PID etc.
@@ -97,4 +98,3 @@ private:
 };
 
 #endif // TARGETCONTROL_H
-
